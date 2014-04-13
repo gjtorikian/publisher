e07aee80f98002342cd9ca94438ceafef9a6ada6
Garen Torikian <gjtorikian@gmail.com>
2014-04-13 01:42:10 -0700

    Set to 2.6 maybe?

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require 'git'
 
 if ENV['IS_HEROKU']
   require 'rubypython'
-  RubyPython.start(:python_exe => "python2.7")
+  RubyPython.start(:python_exe => "python2.6")
 end
 
 class Publisher < Sinatra::Base
