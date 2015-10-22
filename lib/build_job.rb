6b0d2ba1104566a0554b33d4f3cec1c94deaee74
Garen Torikian <gjtorikian@gmail.com>
2015-10-21 21:29:21 -0700

    Incorporate refactor

--- lib/build_job.rb
+++ lib/build_job.rb
@@ -1,37 +1,17 @@
 require 'git'
+require 'octokit'
+require_relative 'cloner'
 
 class BuildJob
   @queue = :default
 
-  def self.perform(tmpdir, token, repo, lang=nil)
-    clone_repo(repo, token, tmpdir)
-    Dir.chdir "#{tmpdir}/#{repo}" do
-      setup_git
-      Bundler.with_clean_env do
-        # fetch gh-pages then hop back to master
-        puts `git fetch --all`
-        @git_dir.branch('gh-pages').checkout
-        @git_dir.branch('master').checkout
-        puts "Installing gems..."
-        puts `bundle install`
-        puts "Publishin'..."
-        puts `bundle exec rake publish[true]`
-        puts "Published!"
-      end
-    end
-  end
+  def self.perform(sha, originating_hostname, originating_repo)
+    cloner = Cloner.new({
+      :sha                   => sha,
+      :originating_hostname  => originating_hostname,
+      :originating_repo      => originating_repo
+    })
 
-  def self.clone_repo(repo, token, tmpdir)
-    puts "Cloning #{repo}..."
-    @git_dir = Git.clone(clone_url_with_token(token, repo), "#{tmpdir}/#{repo}")
-  end
-
-  def self.setup_git
-   @git_dir.config('user.name', 'Hubot')
-   @git_dir.config('user.email', 'cwanstrath+hubot@gmail.com')
-  end
-
-  def self.clone_url_with_token(token, repo)
-    "https://#{token}:x-oauth-basic@github.com/#{repo}.git"
+    cloner.clone
   end
 end
---
+++ lib/cloner.rb
@@ -0,0 +1,140 @@
+require 'open3'
+
+class Cloner
+  GITHUB_DOMAIN = 'github.com'
+
+  DEFAULTS = {
+    :tmpdir               => nil,
+    :sha                  => nil,
+    :sync_method          => 'merge',
+    :originating_hostname => GITHUB_DOMAIN,
+    :originating_repo     => nil,
+    :git                  => nil
+  }
+
+  attr_accessor :tmpdir, :sha, :originating_hostname, :originating_repo
+
+  def initialize(options)
+    logger.level = Logger::WARN if ENV['RACK_ENV'] == 'test'
+    logger.info 'New Cloner instance initialized'
+
+    DEFAULTS.each { |key,value| instance_variable_set("@#{key}", options[key] || value) }
+    @tmpdir ||= Dir.mktmpdir('publisher')
+
+    if originating_hostname != GITHUB_DOMAIN
+      Octokit.configure do |c|
+        c.api_endpoint = "https://#{originating_hostname}/api/v3/"
+        c.web_endpoint = "https://#{originating_hostname}"
+      end
+    end
+
+    git_init
+
+    DEFAULTS.each { |key, _| logger.info "  * #{key}: #{instance_variable_get("@#{key}")}" }
+  end
+
+  def clone
+    Bundler.with_clean_env do
+      Dir.chdir "#{tmpdir}/#{originating_repo}" do
+        add_remote
+        fetch
+        checkout
+        build_docs
+        logger.info 'Published!'
+      end
+    end
+  rescue StandardError => e
+    logger.warn e
+    raise
+  ensure
+    FileUtils.rm_rf(tmpdir)
+    logger.info "Cleaning up #{tmpdir}"
+  end
+
+  def originating_token
+    @originating_token ||= (originating_hostname == GITHUB_DOMAIN ? dotcom_token : ghe_token)
+  end
+
+  def dotcom_token
+    ENV['DOTCOM_MACHINE_USER_TOKEN']
+  end
+
+  def ghe_token
+    ENV['GHE_MACHINE_USER_TOKEN']
+  end
+
+  def remote_name
+    @remote_name ||= "otherrepo-#{Time.now.to_i}"
+  end
+
+  def url_with_token
+    @url_with_token ||= "https://#{originating_token}:x-oauth-basic@#{originating_hostname}/#{originating_repo}.git"
+  end
+
+  # Plumbing methods
+
+  def logger
+    @logger ||= Logger.new(STDOUT)
+  end
+
+  def client
+    @client ||= Octokit::Client.new(:access_token => originating_token)
+  end
+
+  def git
+    @git ||= begin
+      logger.info "Cloning #{originating_repo} from #{originating_hostname}..."
+      Git.clone(url_with_token, "#{tmpdir}/#{originating_repo}")
+    end
+  end
+
+  def run_command(*args)
+    logger.info "Running command #{args.join(' ')}"
+    output = status = nil
+    output, status = Open3.capture2e(*args)
+    output = output.gsub(/#{dotcom_token}/, '<TOKEN>') if dotcom_token
+    logger.info "Result: #{output}"
+    if status != 0
+      report_error(output)
+      fail "Command `#{args.join(' ')}` failed: #{output}"
+    end
+    output
+  end
+
+  def report_error(command_output)
+    body = "Hey, I'm really sorry about this, but there was some kind of error "
+    body << "when I tried to build from #{sha}:\n"
+    body << "\n```\n"
+    body << command_output
+    body << "\n```\n"
+    body << "You'll have to resolve this problem manually, I'm afraid.\n"
+    body << "![I'm so sorry](http://media.giphy.com/media/NxKcqJI6MdIgo/giphy.gif)"
+    client.create_issue originating_repo, '[Publisher] Error detected', body
+  end
+
+  # Methods that perform sync actions, in order
+
+  def git_init
+    git.config('user.name',  ENV['MACHINE_USER_NAME'])
+    git.config('user.email', ENV['MACHINE_USER_EMAIL'])
+  end
+
+  def add_remote
+    logger.info "Adding remote for #{originating_repo} on #{originating_hostname}..."
+    git.add_remote(remote_name, url_with_token)
+  end
+
+  def fetch
+    logger.info "Fetching #{originating_repo}..."
+    git.remote(remote_name).fetch
+  end
+
+  def checkout
+    logger.info "Checking out #{branch_name}"
+    git.branch(branch_name).checkout
+  end
+
+  def build_docs
+     puts `bundle exec rake publish[true]`
+  end
+end
