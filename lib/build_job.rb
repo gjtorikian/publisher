3009e667536417e3341ce7b1ee8bc29bfb6ff73b
Garen Torikian <gjtorikian@gmail.com>
2015-10-21 21:56:04 -0700

    More logging

--- lib/build_job.rb
+++ lib/build_job.rb
@@ -6,6 +6,8 @@ class BuildJob
   @queue = :default
 
   def self.perform(sha, originating_hostname, originating_repo)
+    logger.info "Cloning #{sha} from #{originating_repo} on #{originating_hostname}"
+
     cloner = Cloner.new({
       :sha                   => sha,
       :originating_hostname  => originating_hostname,
