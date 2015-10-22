6914247d67fcc254a1016dad8ff19571aa36693f
Garen Torikian <gjtorikian@gmail.com>
2015-10-21 21:29:21 -0700

    Incorporate refactor

--- lib/app.rb
+++ lib/app.rb
@@ -3,79 +3,50 @@ begin
 rescue LoadError
 end
 
-require "sinatra/base"
-require "json"
-require "fileutils"
-require "octokit"
-require "resque"
-require "redis"
-require "openssl"
-require "base64"
+require 'sinatra/base'
+require 'json'
+require 'redis'
+require 'openssl'
+require 'base64'
 
-require './build_job'
+require_relative '../config/redis'
+require_relative './helpers'
+require_relative './cloner'
 
 class Publisher < Sinatra::Base
   set :root, File.dirname(__FILE__)
+  Dotenv.load if Sinatra::Base.development?
 
   configure do
     configure_redis
   end
 
-  before do
+  get '/' do
+    'I think you misunderstand how to use this.'
+  end
+
+  post '/build' do
     # trim trailing slashes
     request.path_info.sub! %r{/$}, ''
-    pass unless %w[build].include? request.path_info.split('/')[1]
-    # ensure signature is correct
+
+    # ensure there's a payload
     request.body.rewind
-    payload_body = request.body.read
-    verify_signature(payload_body)
+    payload_body = request.body.read.to_s
+    halt 400, 'Missing body payload!' if payload_body.nil? || payload_body.empty?
+
+    # ensure signature is correct
+    github_signature = request.env['HTTP_X_HUB_SIGNATURE']
+    halt 400, 'Signatures didn\'t match!' unless signatures_match?(payload_body, github_signature)
+
+    @payload = JSON.parse(payload_body)
+    halt 202, "Payload was not for master, was for #{@payload['ref']}, aborting." unless master_branch?(@payload)
+
     # keep some important vars
-    @payload = JSON.parse params[:payload]
-    @repo = "#{@payload["repository"]["owner"]["name"]}/#{@payload["repository"]["name"]}"
-    @lang = params[:lang]
-    check_params params
+    process_payload(@payload)
+    @destination_hostname = params[:destination_hostname] || 'github.com'
+
+    Resque.enqueue(BuildJob, @sha, @originating_hostname, @originating_repo)
   end
 
-  get "/" do
-    "I think you misunderstand how to use this."
-  end
-
-  post "/build" do
-    do_the_work
-  end
-
-  helpers do
-
-    def verify_signature(payload_body)
-      signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new("sha1"), ENV['SECRET_TOKEN'], payload_body)
-      return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
-    end
-
-    def check_params(params)
-      return halt 202, "Payload was not for master, aborting." unless master_branch?(@payload)
-    end
-
-    def token
-      ENV["MACHINE_USER_TOKEN"]
-    end
-
-    def master_branch?(payload)
-      payload["ref"] == "refs/heads/master"
-    end
-
-    def do_the_work
-      in_tmpdir do |tmpdir|
-        Resque.enqueue(BuildJob, tmpdir, token, @repo, @lang)
-      end
-    end
-
-    def in_tmpdir
-      path = File.expand_path "#{Dir.tmpdir}/publisher/repos/#{Time.now.to_i}#{rand(1000)}/"
-      FileUtils.mkdir_p path
-      puts "Directory created at: #{path}"
-      yield path
-    ensure
-      FileUtils.rm_rf( path ) if File.exists?( path ) && !Sinatra::Base.development?
-    end
-  end
+  helpers Helpers
 end
---
+++ lib/helpers.rb
@@ -0,0 +1,20 @@
+require 'fileutils'
+require_relative './build_job'
+
+module Helpers
+  def signatures_match?(payload_body, github_signature)
+    return true if Sinatra::Base.development?
+    signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], payload_body)
+    Rack::Utils.secure_compare(signature, github_signature)
+  end
+
+  def process_payload(payload)
+    @originating_repo = "#{payload['repository']['owner']['name']}/#{payload['repository']['name']}"
+    @originating_hostname = payload['repository']['url'].match(%r{//(.+?)/})[1]
+    @after_sha = payload['after']
+  end
+
+  def master_branch?(payload)
+    payload['ref'] == 'refs/heads/master'
+  end
+end
