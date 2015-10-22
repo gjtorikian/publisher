799865227936efe1142759de7177712f1e7f1f1b
Garen Torikian <gjtorikian@gmail.com>
2015-10-21 21:33:37 -0700

    Add tests

---
+++ spec/accepting_parameters_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe 'endpoints' do
+  let(:helpers) { TestHelper.new }
+  let(:incoming) { fixture('incoming.json') }
+  let(:non_master_payload) { incoming.sub('refs/heads/master', 'refs/heads/gh-pages') }
+
+  before do
+    allow_any_instance_of(app).to receive(:signatures_match?).and_return(true)
+    ResqueSpec.reset!
+  end
+
+  describe 'sync' do
+    it 'does nothing without a body' do
+      expect(app).to_not receive(:process_payload)
+      post '/build'
+      expect(last_response.status).to eql(400)
+      expect(last_response.body).to eql('Missing body payload!')
+    end
+
+    it 'does nothing if payload is not for master' do
+      expect(app).to_not receive(:process_payload)
+      post '/build', non_master_payload
+      expect(last_response.status).to eql(202)
+      expect(last_response.body).to eql('Payload was not for master, was for refs/heads/gh-pages, aborting.')
+    end
+
+    it 'can work' do
+      post '/build', incoming
+      expect(last_response.status).to eql(200)
+      expect(BuildJob).to have_queue_size_of(1)
+    end
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,65 @@
+
+ENV['RACK_ENV'] = 'test'
+
+require 'bundler/setup'
+require 'rack/test'
+require 'rspec'
+require 'webmock/rspec'
+require_relative '../lib/app'
+require 'resque_spec'
+require 'fileutils'
+
+WebMock.disable_net_connect!
+
+module RSpecMixin
+  include Rack::Test::Methods
+  def app
+    Publisher
+  end
+end
+
+RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color = true
+
+  # Run in a random order
+  config.order = :random
+
+  config.include RSpecMixin
+end
+
+class TestHelper
+  include Helpers
+end
+
+def with_env(key, value)
+  old_env = ENV[key]
+  ENV[key] = value
+  yield
+  ENV[key] = old_env
+end
+
+def fixture_path(fixture)
+  File.expand_path "fixtures/#{fixture}", File.dirname(__FILE__)
+end
+
+def fixture(fixture)
+  File.open(fixture_path(fixture)).read
+end
+
+def temp_repo
+  File.expand_path(File.join('spec/fixtures/working.git'))
+end
+
+def octokit_version
+  Gem.loaded_specs['octokit'].version
+end
+
+def tmpdir
+  File.expand_path "../tmp", File.dirname(__FILE__)
+end
+
+def setup_tmpdir
+  FileUtils.rm_rf tmpdir
+  FileUtils.mkdir tmpdir
+end
