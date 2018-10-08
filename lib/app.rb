c78e37b19240c764975bed4e66641e2c780697ce
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2018-10-07 20:27:05 -0400

    Update README

--- lib/app.rb
+++ lib/app.rb
@@ -50,7 +50,7 @@ class Publisher < Sinatra::Base
     process_payload(@payload)
 
     # query parameter specifying additional users or teams to ping in the error issue.
-    @cc_on_error = params[:cc_on_error].split(',').map { |user_or_team| '@' + user_or_team } if params[:cc_on_error]
+    @cc_on_error = params[:cc_on_error].split(',').map { |user_or_team| "@#{user_or_team}" } if params[:cc_on_error]
 
     @app_client = configure_app_client
     Resque.enqueue(BuildJob, @app_client, @committer, @sha, @originating_hostname, @originating_repo, @cc_on_error)
